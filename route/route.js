c36d0572aaed633ff7a9fbb20972e7b8662a04ca
Danilo Titato <danilotitato@gmail.com>
2021-04-24 23:01:43 -0300

    Added random word querying endpoint

--- route/route.js
+++ route/route.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const Word = require('../model/word.js')
 
-router.get('/word', (req, res) => {
-    res.send({future:'Get Word and Definition'});
-})
+router.get('/word', (req, res, next) => {
+    Word.aggregate( [{ $sample: {size:1} }] ).then (function(word) {
+        console.log(word);
+        res.send(word);
+    }).catch(next);
+});
 
 module.exports = router;
