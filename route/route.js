e8e1168a44b1864ecaf593aee2594755c95ba1ef
Danilo Titato <danilotitato@gmail.com>
2021-04-25 00:10:13 -0300

    Added a word search to the dictionary

--- route/route.js
+++ route/route.js
@@ -2,11 +2,26 @@ const express = require('express');
 const router = express.Router();
 const Word = require('../model/word.js')
 
-router.get('/word', (req, res, next) => {
-    Word.aggregate( [{ $sample: {size:1} }] ).then (function(word) {
+router.get('/random', (req, res, next) => {
+    Word.aggregate( [{ $sample: {size:1} }] ).then(word => {
         console.log(word);
         res.send(word);
     }).catch(next);
+});
+
+router.get('/search', (req, res) => {
+    query = req.query.query;
+    console.log(query);
+
+    Word.findOne({word: query}).then(query => {
+        if (!query) {
+            console.log("not found!");
+            res.status(404).send("not found!");
+        } else {
+            console.log("found!");
+            res.send(query);
+        }
+    });
 });
 
 module.exports = router;
