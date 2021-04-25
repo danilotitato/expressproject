0e4381307696dd1a4243a1c3607263e9bceb98ee
Danilo Titato <danilotitato@gmail.com>
2021-04-24 23:00:42 -0300

    Setup mongoose

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 const port = process.env.HTTP_PORT || 3000;
 
+let mongoDB = 'mongodb://127.0.0.1/vocab';
+mongoose.connect(mongoDB, {useNewUrlParser:true, useUnifiedTopology:true});
+
 app.use(express.json());
-app.use('/api', require('./route/route'));
+app.use('/api', require('./route/route.js'));
 
 app.listen(port, () => {
     console.log('Server listening on port ' + port);
