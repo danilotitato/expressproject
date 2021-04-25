d8edb5be7aada7b866a4cb0fa3a6257647a0c234
Danilo Titato <danilotitato@gmail.com>
2021-04-25 18:50:08 -0300

    Improved mongoose connection promise handling

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const app = express();
-const port = process.env.HTTP_PORT || 3000;
 
 let mongoDB = 'mongodb://127.0.0.1/vocab';
-mongoose.connect(mongoDB, {useNewUrlParser:true, useUnifiedTopology:true});
+mongoose
+    .connect(mongoDB, {useNewUrlParser:true, useUnifiedTopology:true})
+    .then(() => {
+        const app = express();
+        const port = process.env.HTTP_PORT || 3000;
 
-app.use(express.json());
-app.use('/api', require('./route/route.js'));
+        app.use(express.json());
+        app.use('/api', require('./route/route.js'));
 
-app.listen(port, () => {
-    console.log('Server listening on port ' + port);
-})
+        app.listen(port, () => {
+            console.log('Server listening on port ' + port);
+        })
+    });
